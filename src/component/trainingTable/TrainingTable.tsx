6b95a5c378570042a1d0667d41dad9d6248436c6
Pawel Dudek <pawel.dudek@cgi.com>
2021-09-02 14:48:16 +0200

    added react bootstrap

--- src/component/trainingTable/TrainingTable.tsx
+++ src/component/trainingTable/TrainingTable.tsx
@@ -1,10 +1,41 @@
 import { ITrainingTableProps } from "./TrainingTableModel";
+import { Table } from 'react-bootstrap';
 
 const TrainingTable = (props: ITrainingTableProps) => {
     console.log('Trainig table', props.training)
 
     return(
-        <div></div>
+        <div>
+            <Table striped bordered hover variant="dark">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</Table>
+        </div>
     )
 }
 
