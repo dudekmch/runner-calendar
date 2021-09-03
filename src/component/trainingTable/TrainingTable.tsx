1f957cd4a398d2f65f20228e4781e567e74ac7fc
Pawel Dudek <pawel.dudek@cgi.com>
2021-09-04 00:52:06 +0200

    added method to get distinct set of all interval types

--- src/component/trainingTable/TrainingTable.tsx
+++ src/component/trainingTable/TrainingTable.tsx
@@ -4,13 +4,19 @@ import { ITraining } from "../../model/Training";
 import TrainingTableRow from "./trainingTableRow/TrainingTableRow";
 
 const TrainingTable = (props: ITrainingTableProps) => {
-  console.log(props.training)
-
   const createRows = (training: ITraining) => {
     if (training && training.data) {
-      return props.training.data.map((item) => (
-        <TrainingTableRow row={item}/>
-      ));
+      getDistinctIntervalTypes();
+      return props.training.data.map((item) => <TrainingTableRow row={item} />);
+    }
+  };
+
+  const getDistinctIntervalTypes = () => {
+    if (props.training && props.training.data) {
+      const types = new Set();
+      props.training.data.forEach((interval) => types.add(interval.type));
+      console.log(types);
+      return types;
     }
   };
 
